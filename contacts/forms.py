b120de59cf51eebcbcc446ca61cd50c296eb074b
Kevin <kecasper@gmail.com>
2018-11-12 13:22:28 +0100

    añadir contacto ajax

--- contacts/forms.py
+++ contacts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Contact
 
+
 class ContactForm(forms.ModelForm):
 
     class Meta:
@@ -15,5 +16,9 @@ class ContactForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(ContactForm, self).__init__(*args, **kwargs)
+        self.fields['nameContact'].widget.attrs['id'] = 'nameContact'
+        self.fields['email'].widget.attrs['id'] = 'email'
+        self.fields['phone'].widget.attrs['id'] = 'phone'
+        self.fields['message'].widget.attrs['id'] = 'message'
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'form-control'
